0fa3802882436f60327ec5b46434086ddca62b1f
taskovikj <branislav.takovikj@gmail.com>
2021-11-30 19:21:50 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardDemoComponent } from './card-demo/card-demo.component';
 import {MatCardModule} from '@angular/material/card';
-
+import { ImgSliderComponent } from './img-slider/img-slider.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardDemoComponent
+    CardDemoComponent,
+    ImgSliderComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     MatSliderModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
